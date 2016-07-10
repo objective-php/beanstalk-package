b8bda226bec801d50b36ae677289af113bc4fefa
Gauthier Delamarre <gauthier@objective-php.org>
2016-07-10 16:02:38 +0200

    Initial release

---
+++ src/BeanstalkPackage.php
@@ -0,0 +1,84 @@
+<?php
+    /**
+     * This file is part of the Objective PHP project
+     *
+     * More info about Objective PHP on www.objective-php.org
+     *
+     * @license http://opensource.org/licenses/GPL-3.0 GNU GPL License 3.0
+     */
+    
+    namespace ObjectivePHP\Package\Beanstalk;
+    
+    
+    use ObjectivePHP\Application\ApplicationInterface;
+    use ObjectivePHP\Application\Middleware\AbstractMiddleware;
+    use ObjectivePHP\Package\Beanstalk\Config\BeanstalkServer;
+    use ObjectivePHP\ServicesFactory\Specs\PrefabServiceSpecs;
+    use Pheanstalk\Pheanstalk;
+
+    /**
+     * Class BeanstalkPackage
+     *
+     * @package ObjectivePHP\Package\Beanstalk
+     */
+    class BeanstalkPackage extends AbstractMiddleware
+    {
+
+        /**
+         * Beanstalk services common prefix
+         */
+        const SERVICE_PREFIX = 'beanstalk.';
+
+
+        /**
+         * @var string
+         */
+        protected $bootstrapStep;
+
+
+        /**
+         * BeanstalkPackage constructor.
+         *
+         * @param string $bootstrapStep
+         */
+        public function __construct($bootstrapStep = 'bootstrap')
+        {
+            $this->bootstrapStep = $bootstrapStep;
+        }
+    
+    
+        /**
+         * @param ApplicationInterface $app
+         *
+         * @throws Exception
+         */
+        public function run(ApplicationInterface $app)
+        {
+            if(empty($app->getSteps()[$this->bootstrapStep]))
+            {
+                throw new Exception(sprintf('Cannot plug Beanstalk services registration to specified application step: "%s" because this step has not been defined.', $this->bootstrapStep));
+            }
+
+            $app->getStep($this->bootstrapStep)->plug([$this, 'registerServices']);
+        }
+
+        /**
+         * @param ApplicationInterface $app
+         */
+        protected function registerServices(ApplicationInterface $app)
+        {
+            $servers = $app->getConfig()->subset(BeanstalkServer::class);
+    
+            foreach ($servers as $service => $data)
+            {
+        
+                $serviceName = self::SERVICE_PREFIX . $service;
+        
+                $client = new Pheanstalk($data['host'], $data['port'], $data['timeout'], $data['persistent']);
+                $client->useTube($data['tube']);
+        
+                $app->getServicesFactory()->registerService(new PrefabServiceSpecs($serviceName, $client));
+            }
+        }
+    
+    }
---
+++ src/Config/BeanstalkServer.php
@@ -0,0 +1,45 @@
+<?php
+    /**
+     * This file is part of the Objective PHP project
+     *
+     * More info about Objective PHP on www.objective-php.org
+     *
+     * @license http://opensource.org/licenses/GPL-3.0 GNU GPL License 3.0
+     */
+    
+    namespace ObjectivePHP\Package\Beanstalk\Config;
+    
+    
+    use ObjectivePHP\Config\Exception;
+    use ObjectivePHP\Config\SingleValueDirectiveGroup;
+    use ObjectivePHP\Primitives\Collection\Collection;
+
+    class BeanstalkServer extends SingleValueDirectiveGroup
+    {
+        /**
+         * BeanstalkServer constructor.
+         *
+         * @param $identifier
+         * @param $value array Server configuration
+         *
+         * @throws Exception
+         */
+        public function __construct($identifier, $value)
+        {
+            // check value
+            $value = Collection::cast($value)->toArray();
+
+            $mandatory = ['host', 'tube'];
+
+            $missing = array_diff($mandatory, array_keys($value));
+
+            if($missing)
+            {
+                throw new Exception(sprintf('Missing %s keys in beanstalk configuration', implode(', ', $missing)));
+            }
+
+            parent::__construct($identifier, $value);
+        }
+
+
+    }
